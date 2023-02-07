b4d7ab53cb3976809aee74cd434d099d472554b2
krocksa <rogeryonatan@gmail.com>
2023-02-06 23:46:37 -0400

    FunctionalComponent

--- src/Components/FunctionalComponent.tsx
+++ src/Components/FunctionalComponent.tsx
@@ -9,7 +9,7 @@ interface FunctionalComponentProps{
 
 export function FunctionalComponent (props: FunctionalComponentProps): JSX.Element {
     return <>
-        Hola, {props.name}
+       <h1 className="FC">Hola, {props.name}</h1>
     </>
 }
 
